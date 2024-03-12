1cce7972af371535b6a80def05e76e64f1bf655c
astromcquinn <mcquinn@uw.edu>
2024-03-11 17:02:00 -0700

    removing read_gadget.py

--- read_gadget.py
+++
@@ -1,6 +0,0 @@
-import bigfile
-from fake_spectra import spectra
-bf = bigfile.BigFile(“PART 001”) pos = bf[“1/Position”][:]
-rates = GasProperties(redshift, snap, hubble, units=unit)
-temp = rates.get_temp(0, -1)
-dens = rates.get_code_rhoH(0, -1)
